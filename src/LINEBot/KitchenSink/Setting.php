82e9d25e24a6baf0c56d409c1ddf98e869c032ee
poteii <poteii.pasel@icloud.com>
2017-08-18 14:08:03 +0700

    Add files via upload

---
+++ src/LINEBot/KitchenSink/Setting.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace LINE\LINEBot\KitchenSink;
+
+class Setting
+{
+    public static function getSetting()
+    {
+        return [
+            'settings' => [
+                'displayErrorDetails' => true, // set to false in production
+
+                'logger' => [
+                    'name' => 'slim-app',
+                    'path' => __DIR__ . '/../../../logs/app.log',
+                ],
+
+                'bot' => [
+                    'channelToken' => getenv('LINEBOT_CHANNEL_TOKEN') ?: 'nh7BWpnKFdxiz9UTcB3HttsbbBC9DIxMHeQUGznWDqLQ6yAyM9iyYSqn6BO4Yg+6NxNqZA3ZDmcu/1O7RTO0SL4vUdQdoTgOkQCVJP8Qm5O2ivj40/ezbv/n51ekqhaFUGl47j8Yvx8ChjaEhfH/DAdB04t89/1O/w1cDnyilFU=',
+                    'channelSecret' => getenv('LINEBOT_CHANNEL_SECRET') ?: '1185659edeea8401839344f51c9fa15f',
+                ],
+
+                'apiEndpointBase' => getenv('LINEBOT_API_ENDPOINT_BASE'),
+            ],
+        ];
+    }
+}
